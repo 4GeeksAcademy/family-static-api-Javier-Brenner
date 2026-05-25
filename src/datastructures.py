fac04e692ac0420f1c779ea6dee4e85820bce035
JavierBrenner <javibrenner18@gmail.com>
2026-05-25 17:32:03 +0000

    Family Structure added

--- src/datastructures.py
+++ src/datastructures.py
@@ -9,7 +9,31 @@ class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
         self._next_id = 1
-        self._members = []
+        self._members = [
+            {
+                "id": self._generate_id(),
+                "first_name": "John",
+                "last_name": last_name,
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # This method generates a unique incremental ID
     def _generate_id(self):
